73422fa2ea58104d06fbcc548cdf1e4cbbd95012
Jack Watt <jsw.watt@gmail.com>
2026-05-22 18:08:31 -0300

    Delete kata/hacker_rank/python directory

--- kata/hacker_rank/python/introduction/print.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    out = ""
-    for x in range (n):
-        out += str(x+1)
-    print(out)
--- kata/hacker_rank/python/strings/what_is_your_name.py
+++
@@ -1,2 +0,0 @@
-def print_full_name(a, b):
-    print("Hello " + str(a) + " " + str(b) +"! You just delved into python." )
